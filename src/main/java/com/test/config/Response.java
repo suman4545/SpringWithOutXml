b3558fba907efb81482d5b7f57e1f3905de96ef7
suman4545 <smn.bsws@gmail.com>
2019-07-30 21:23:18 +0530

    commit to local

---
+++ src/main/java/com/test/config/Response.java
@@ -0,0 +1,24 @@
+package com.test.config;
+
+public class Response {
+	private String msg;
+	private String code;
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+	
+	 
+}
---
+++ src/main/java/com/test/config/TestController.java
@@ -0,0 +1,34 @@
+package com.test.config;
+
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@GetMapping(path="/getres",produces=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Response>getData(){
+		Response res=new Response();
+		res.setCode("100");
+		res.setMsg("Hi Spring WithOut Xml.This is a get method");
+		return ResponseEntity.ok(res);
+		
+	}
+	
+	@PostMapping(path="/postdata",produces=MediaType.APPLICATION_JSON_VALUE,consumes=MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Response>PostData(RequestEntity<Request>reqBody){
+		Response res=new Response();
+		res.setCode("100");
+		res.setMsg("Hi Spring WithOut Xml .Posted Data:"+reqBody.getBody().getName());
+		return ResponseEntity.ok(res);
+		
+	}
+	
+	
+	
+
+}
